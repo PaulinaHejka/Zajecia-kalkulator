a39310af4cb379ccd59497c1692a4d8646583c31
Paulina Szablewska <52970@student.ue.poznan.pl>
2019-04-08 17:35:00 +0200

    Funkcja pomocy

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,11 @@
 print('To jest prosty kalkulator!')
+def gethelp():
+	print("Podaj dwie liczby:")
+
 def dodaj():
 	a = int(input())
 	b = int(input())
 	print(a+b)
 
+gethelp()
 print('Koniec programu! :)')
